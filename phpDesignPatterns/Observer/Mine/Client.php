b20f458bd35960887d0314161441a52a1302d6b0
Bayon <bforte@indatus.com>
2014-03-17 14:34:14 -0400

    new observer pattern

---
+++ phpDesignPatterns/Observer/Mine/Client.php
@@ -0,0 +1,17 @@
+<?php
+ 
+include_once("Observer.php");
+include_once("Subject.php");
+
+
+$subject = new Subject();
+$observer = new Observer();
+
+
+$subject->attach($observer);
+
+$subject->changeTopic("asdf,factory,strategy,monkeys");
+
+$subject->changeTopic("fleas in the tea room");
+
+?>
